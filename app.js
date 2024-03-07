2444777c61689650f3c3490a834b39b2cd609c39
farid123d <117964049+farid123d@users.noreply.github.com>
2024-03-07 23:13:10 +0800

    final-commit

--- app.js
+++ app.js
@@ -7,16 +7,31 @@ const expressionRoute = require('./router/expression.router')
 
 app.use(expressionRoute)
 
+const middleware = (req, res, next) =>{
+   console.log('Hello middle ware')
+   next();
+}
+
+app.use(middleware)
+
+app.use(express.static('public'))
 
 // create the home page
-app.get('/',(req, res) =>{
+app.get('/', (req, res) =>{
+   console.log('Home page')
    res.send('<h2>Home page</h2>')
+})
+
+app.get('/about', (req, res) => {
+   res.sendFile(__dirname + '/view/index.html')
 })
 
 // create the not found page
 app.use((req, res) =>{
    res.send('<h2>404 !!! Not found page </h2>')
 })
+
+
 
 
 // export the file
